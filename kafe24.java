ee5f0cee3ba1b6545bc70d50f3feba442e0ceb15
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-09-26 09:39:36 +0700

    percobaan 3 job sheet 5

---
+++ kafe24.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class kafe24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String menu;
+        char ukuranCup;
+        int jumlah;
+        boolean keanggotaan;
+
+        System.out.println("Masukkan Menu : ");
+        menu = sc.nextLine();
+        System.out.println("Masukkan Ukuran Cup : ");
+        ukuranCup = sc.next().charAt(0);
+        System.out.println("Masukkan Jumlah Pembelian : ");
+        jumlah = sc.nextInt();
+        System.out.println("Masukkan Keanggotaan (True/false)");
+        keanggotaan = sc.nextBoolean();
+
+        double hargaMenu = 0;
+        switch (menu.toLowerCase()) {
+            case "kopi":
+                hargaMenu = 12000; 
+                break;
+            case "teh":
+                hargaMenu = 7000;
+                break;
+            case "coklat":
+                hargaMenu = 20000;
+                break;
+        }
+        double totalHarga = hargaMenu * jumlah;
+
+        switch (ukuranCup) {
+            case 'S':
+                break;
+            case 'M':
+            totalHarga += 0.25 * totalHarga;
+                break;
+            case 'L':
+            totalHarga += 0.4 * totalHarga;
+                break;
+            default:
+                System.out.println("Ukuran cup tidak valid.");
+                return;
+        }
+
+        double diskon = keanggotaan ? 0.1 : 0;
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+        
+        System.out.println("Item Pembelian " + jumlah + " " + menu + " dengan ukuran cup " + ukuranCup);
+        System.out.println("Nominal  Bayar : " + nominalBayar);
+    }
+}
